623fbe9ab89b1fda226adccc33d4dd48c7a52fb3
florozco <florozco@uninorte.edu.co>
2021-09-06 20:21:07 -0500

    Image index js
    se creo index.js dentro de la carpeta image para referenciar mas facilmente los iconos

--- src/App.js
+++ src/App.js
@@ -1,32 +1,23 @@
-
-import './App.css';
-import Sidebar from './Components/Sidebar/Sidebar';
-import Feed from './Components/Feed/Feed'
-import {Helmet} from "react-helmet";
-import Widgets from './Components/Widgets/Widgets'
+import "./App.css";
+import Sidebar from "./Components/Sidebar/Sidebar";
+import Feed from "./Components/Feed/Feed";
+import { Helmet } from "react-helmet";
+import Widgets from "./Components/Widgets/Widgets";
 
 function App() {
   return (
     <div className="app">
       <Helmet>
-                
-                <title>My Title</title>
-                <meta name="description" content="Twitter" />
-                <meta 
-                property="og:description"
-                content="Sign Up in copy of twiteer"
-                />
-      </Helmet>   
-    {/*Sidebar*/}
-    <Sidebar/>
-    {/*feed*/}
-    <Feed/>
-    {/*Widgets*/}
-    <Widgets/>
-
-    
-    
-    
+        <title>My Title</title>
+        <meta name="description" content="Twitter" />
+        <meta property="og:description" content="Sign Up in copy of twiteer" />
+      </Helmet>
+      {/*Sidebar*/}
+      <Sidebar />
+      {/*feed*/}
+      <Feed />
+      {/*Widgets*/}
+      <Widgets />{" "}
     </div>
   );
 }
--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,19 +1,16 @@
-import React from 'react'
-import  './Feed.css'
-import '../Sidebar/SidebarNav'
-import SidebarNav from '../Sidebar/SidebarNav'
-function Feed() {
-    return (
-        <div className="feed">
-            <div className="feedHeader">
-            <h2>Hola</h2>
-            
-            </div>
+import React from "react";
+import "./Feed.css";
+import "../Sidebar/SidebarNav";
+import SidebarNav from "../Sidebar/SidebarNav";
 
-            
-            
-        </div>
-    )
+function Feed() {
+  return (
+    <div className="feed">
+      <div className="feedHeader">
+        <h2>Hola</h2>
+      </div>
+    </div>
+  );
 }
 
-export default Feed
+export default Feed;
